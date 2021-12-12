3a005d0584dfa16a54670292124a527ed75332b6
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2021-12-12 06:48:18 +0000

    initial commit

---
+++ elpa/srcery-theme-20210601.1247/srcery-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from srcery-theme.el  -*- no-byte-compile: t -*-
+(define-package "srcery-theme" "20210601.1247" "Dark color theme" '((emacs "24")) :commit "58dd21cd63e4a2eed15e0082c2547069363f107b" :authors '(("Daniel Berg")) :maintainer '("Daniel Berg") :keywords '("faces") :url "https://github.com/srcery-colors/srcery-emacs")
